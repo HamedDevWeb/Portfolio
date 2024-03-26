3ab7fd62270c302f667c49da7c3f6a104a16a850
Ahmedgithub@gmx.fr <Ahmedgithub@gmx.fr>
2024-03-26 14:37:44 +0100

    mon premier commit

---
+++ app/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/index.css">
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <header>
+        
+        <nav>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Projets</a></li>
+                <li><a href="">Formations</a></li>
+                <li><a href="">A Propos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="name">
+            <h1 >ATMANE Hamed</h1>
+        </div>
+        <div class="content">
+            <div>
+                <img src="./images/dev.avif" alt="">
+            </div>
+            <h2>Web Developpeur</h2>
+            <h2></h2>
+        </div>
+    </main>
+</body>
+</html>
